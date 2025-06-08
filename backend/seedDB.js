7b07a23922c073857fb3eecc7821d5697854148d
yashdarekar17 <dyash8608@gmail.com>
2025-06-08 15:54:43 -0200

    Update seedDB.js

--- backend/seedDB.js
+++ backend/seedDB.js
@@ -3,7 +3,7 @@ const Food = require('./models/food');
 
 
 
-mongoose.connect('mongodb://localhost:27017/nutrition-db', {
+mongoose.connect(process.env.MONGO_URI, {
     // useNewUrlParser: true,
     // useUnifiedTopology: true
     
